21cdcad0a329842abe8a100e4b44ca7cda6efd0f
bg291029 <bg291029@C32651.ens.uqam.ca>
2017-02-02 21:20:34 -0500

    1er commit

---
+++ Lab_32/src/lab_32/Lab_32.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab_32;
+
+/**
+ *
+ * @author bg291029
+ */
+public class Lab_32 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Clone project");
+    }
+    
+}
